8f1761f22f1f72bcab9f4b2b3b11ea4fa6f23374
Bernadette Diemer <diemer.lulu@gmail.com>
2021-10-02 16:14:24 +0200

    my first CRUD app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # LIST ALL TASKS
+  get 'tasks', to: 'tasks#index'
+
+  # CREATE A NEW TASK
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # UPDATE A TASK
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE A TASK
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+
+  # READ ONE TASK
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
